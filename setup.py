f95c2dfdb8ea4e868845c2dd739817575bb1bcd1
Eunchong Yu <kroisse@gmail.com>
2012-06-04 10:30:31 +0900

    Implement adding a task & showing stack of task

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     version='0.1',
     packages=find_packages(),
     zip_safe=False,
-    install_requires=['Flask', 'redis'],
+    install_requires=['Flask >= 0.8', 'redis >= 2.4.13', 'iso8601'],
 )
--- watchout/__init__.py
+++ watchout/__init__.py
@@ -1,5 +1,20 @@
-from flask import Flask, render_template
+import datetime
+import iso8601
+from flask import Flask, request, render_template, url_for, redirect
 from redis import StrictRedis
+
+
+class UTC(datetime.tzinfo):
+    def utcoffset(self, dt):
+        return datetime.timedelta(0)
+
+    def dst(self, dt):
+        return datetime.timedelta(0)
+
+    def tzname(self, dt):
+        return 'UTC'
+
+utc = UTC()
 
 
 app = Flask(__name__)
@@ -8,4 +23,39 @@ conn = StrictRedis()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    now = datetime.datetime.utcnow().replace(tzinfo=utc)
+    stack_keys = conn.lrange('stack', 0, -1)
+    stack = []
+    for key in stack_keys:
+        created_at = iso8601.parse_date(key[6:])
+        stack.append({'title': conn.get(key + '/title'),
+                      'context': conn.get(key + '/context'),
+                      'created_at': created_at,
+                      'elapsed': now - created_at})
+    stack_top = None
+    if stack:
+        stack_top = stack.pop(0)
+    return render_template('index.html', now=now, stack=stack, top=stack_top)
+
+
+@app.route('/tasks/add')
+def form_add_task():
+    return render_template('add_task.html')
+
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    title = request.form['title']
+    context = request.form.get('context', u'')
+    now = datetime.datetime.utcnow()
+    key = 'tasks/{0}'.format(now.isoformat())
+    conn.set(key + '/title', title)
+    conn.set(key + '/context', context)
+    conn.lpush('stack', key)
+    conn.rpush('archive', key)
+    return redirect(url_for('index'))
+
+
+@app.route('/tasks/')
+def edit_task(id):
+    pass
