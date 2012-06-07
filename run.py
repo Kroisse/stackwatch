936f0821d12e8b3d8ce54f8fbc097f5f2ce2a882
Eunchong Yu <kroisse@gmail.com>
2012-06-08 01:24:31 +0900

    Remove a builted secret key and make the code that creates it for each repo

--- run.py
+++ run.py
@@ -1,7 +1,25 @@
 import os.path
+import random
+import string
 from stackwatch import app
 
-cfg_filepath = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                            'default.cfg'))
+
+def filepath(*args):
+    base_dir = os.path.dirname(__file__)
+    return os.path.abspath(os.path.join(base_dir, *args))
+
+
+def make_secret_key(length):
+    chars = string.digits + string.ascii_letters
+    return ''.join(random.choice(chars) for _ in xrange(length))
+
+
+cfg_filepath = filepath('default.cfg')
+local_cfg = filepath('local.cfg')
+if not os.path.exists(local_cfg):
+    with open(local_cfg, 'w') as f:
+        secret_key = make_secret_key(64)
+        print >> f, "SECRET_KEY = '{0}'".format(secret_key)
 app.config.from_pyfile(cfg_filepath)
+app.config.from_pyfile(local_cfg)
 app.run(debug=True)
