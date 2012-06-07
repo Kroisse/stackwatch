cba7471f3c4d6951d6e32802eb4bf7d53bd6b292
Eunchong Yu <kroisse@gmail.com>
2012-06-07 15:02:09 +0900

    Implement popping/editing a task

--- watchout/__init__.py
+++ watchout/__init__.py
@@ -1,5 +1,5 @@
 import iso8601
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, request, render_template, url_for, redirect, abort
 from redis import StrictRedis
 
 from .util import utcnow
@@ -27,10 +27,12 @@ def index():
     stack = []
     for key in stack_keys:
         created_at = iso8601.parse_date(key[6:])
-        stack.append({'title': conn.get(key + '/title'),
-                      'context': conn.get(key + '/context'),
-                      'created_at': created_at,
-                      'elapsed': now - created_at})
+        stack.append({
+            'id': key.rsplit('/', 1)[-1],
+            'title': unicode(conn.get(key + '/title'), 'utf-8'),
+            'context': unicode(conn.get(key + '/context'), 'utf-8'),
+            'created_at': created_at,
+            'elapsed': now - created_at})
     stack_top = None
     if stack:
         stack_top = stack.pop(0)
@@ -55,6 +57,21 @@ def add_task():
     return redirect(url_for('index'))
 
 
-@app.route('/tasks/')
-def edit_task(id):
-    pass
+@app.route('/tasks/<task_id>/edit', methods=['POST'])
+def edit_task(task_id):
+    key = 'tasks/{0}'.format(task_id)
+    if not conn.exists(key + '/context'):
+        abort(404)
+    context = request.form['context']
+    conn.set(key + '/context', context)
+    return redirect(url_for('index'))
+
+
+@app.route('/tasks/pop', methods=['POST'])
+def pop_task():
+    if conn.llen('stack') <= 0:
+        abort(400)
+    key = conn.lpop('stack')
+    now = utcnow()
+    conn.set(key + '/completed_at', now.isoformat())
+    return redirect(url_for('index'))
